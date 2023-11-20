1a38ae3d340a872c4aa853aadb672d057d23bdfd
srivallli <146004888+srivallli@users.noreply.github.com>
2023-11-20 20:27:35 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     int n,m,k;     scanf("%d %d %d",&n,&m,&k);     if(n<=(m-k))     {         printf("YES ");     }     else     {         printf("NO ");     }     int N,M,K;     scanf("%d %d %d",&N,&M,&K);     if(N<=(M-K))     {         printf("YES");     }     else     {         printf("NO");     }
    }

---
+++ Student_Registration.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    int n,m,k;
+    scanf("%d %d %d",&n,&m,&k);
+    if(n<=(m-k))
+    {
+        printf("YES
+");
+    }
+    else
+    {
+        printf("NO
+");
+    }
+    int N,M,K;
+    scanf("%d %d %d",&N,&M,&K);
+    if(N<=(M-K))
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    
+}
