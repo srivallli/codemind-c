0208dc1c6392d696b70d0e2e49e41baa1c38814e
srivallli <146004888+srivallli@users.noreply.github.com>
2023-11-22 10:59:17 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1 ; i<=n ; i++)     {         int num=1;         for(int j=1 ; j<=n ; j++)         {           printf("%d ",num);           num++;         }         printf(" ");     } }

---
+++ Basic_Pattern_1.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1 ; i<=n ; i++)
+    { 
+        int num=1;
+        for(int j=1 ; j<=n ; j++)
+        {
+          printf("%d ",num);
+          num++;
+        }
+        printf("
+");
+    }
+}
